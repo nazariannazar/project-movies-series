3c7878e7e717a3e0adac359a8173426d361cb0e3
Nazar101968 <68591164+Nazar101968@users.noreply.github.com>
2020-10-22 12:33:05 +0200

    first commit

---
+++ build/precache-manifest.e607045155025bbf406d3ab23c2475b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ab722409defd8a317cb5316c484e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4170f23e5f4cdc32b07",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5b2af44868f3fbbd2d86",
+    "url": "/static/css/main.a7c55e62.chunk.css"
+  },
+  {
+    "revision": "c4170f23e5f4cdc32b07",
+    "url": "/static/js/2.89e844f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.89e844f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2af44868f3fbbd2d86",
+    "url": "/static/js/main.f41165bd.chunk.js"
+  },
+  {
+    "revision": "0459971b64e145b17c20",
+    "url": "/static/js/runtime-main.fa965187.js"
+  },
+  {
+    "revision": "0e8d88c032de674865def29a169c2ba7",
+    "url": "/static/media/app-store.0e8d88c0.svg"
+  },
+  {
+    "revision": "5ba738e89445cdd2a038b8cc996c2877",
+    "url": "/static/media/facebook-blue.5ba738e8.svg"
+  },
+  {
+    "revision": "0ab2d96b0f7299f5fd5a80b0ba4beb61",
+    "url": "/static/media/instagram-blue.0ab2d96b.svg"
+  },
+  {
+    "revision": "e017762f3e7c0198d6a0e54a2c8a0ed9",
+    "url": "/static/media/play-store.e017762f.svg"
+  },
+  {
+    "revision": "247071893d8185da6f590a890ec6b059",
+    "url": "/static/media/twitter-blue.24707189.svg"
+  },
+  {
+    "revision": "f0450282133e83188f1ec9a53092713f",
+    "url": "/static/media/windows-store.f0450282.svg"
+  }
+]);
